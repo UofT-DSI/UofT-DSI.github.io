823bda7d4fa2dd9cc38add5366c4a4f4423d9b5e
Daniel R <daniel.razavi@utoronto.ca>
2024-06-06 11:30:58 -0400

    Adding a side bar mvp

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,3 @@
-// src/pages/Home.tsx
 import React, { useState } from 'react';
 import StudentCard from '../components/student_card/student_card';
 import StudentDetailsModal from '../components/student_details_modal/student_details_modal';
@@ -15,7 +14,6 @@ interface Student {
     github: string;
 }
 
-
 const Home: React.FC = () => {
 
     const [selectedStudent, setSelectedStudent] = useState<Student | null>(null);
@@ -29,20 +27,36 @@ const Home: React.FC = () => {
     };
 
     return (
-        <div className={styles.student_grid}>
-            {students.map((student: Student) => (
-                <StudentCard
-                    key={student.id}
-                    student={student}
-                    onSelect={() => handleSelectStudent(student)}
-                />
-        ))}
-        {selectedStudent && (
-            <StudentDetailsModal
-            student={selectedStudent}
-            onClose={handleCloseModal}
-        />
-        )}
+        <div className={styles.container}>
+            <div className={styles.sidebar}>
+                {/* Placeholders for sorting and filtering options */}
+                <p>Sorting</p>
+                <button className={styles.sidebutton} onClick={() => console.log('Sort by Name')}>Sort by Name</button>
+                <button className={styles.sidebutton} onClick={() => console.log('Sort by Certification')}>Sort by Certification</button>
+                <p>Have Linkedin?</p>
+                <button className={styles.sidebutton} onClick={() => console.log('Sort by Name')}>Yes</button>
+                <button className={styles.sidebutton} onClick={() => console.log('Sort by Name')}>No</button>
+                <p>Have GitHub?</p>
+                <button className={styles.sidebutton} onClick={() => console.log('Sort by Name')}>Yes</button>
+                <button className={styles.sidebutton} onClick={() => console.log('Sort by Name')}>No</button>
+                
+                {/* Add more sorting and filtering options as needed */}
+            </div>
+            <div className={styles.student_grid}>
+                {students.map((student: Student) => (
+                    <StudentCard
+                        key={student.id}
+                        student={student}
+                        onSelect={() => handleSelectStudent(student)}
+                    />
+                ))}
+                {selectedStudent && (
+                    <StudentDetailsModal
+                        student={selectedStudent}
+                        onClose={handleCloseModal}
+                    />
+                )}
+            </div>
         </div>
     );
 };
